397b1a01251b1eb31e5295454d9ae2661e272e06
AndyValverde <lupy29@hotmail.com>
2021-01-27 23:18:59 -0600

    Importar archivo de nuestro propio proyecto

--- Bases/app.js
+++ Bases/app.js
@@ -1,20 +1,8 @@
-//require
-const fs = require('fs');
-//datos
+//required nuestro
+//{} es para destructuración!!!
+let {crearArchivo} = require('./multiplicar/multiplicar');
 let base = 6;
-let data ='';
-for(let i = 1; i<= 10; i++){
-    //esto guarda las multiplicaciones
-data += (`${base} * ${i} = ${base*i} \n`);
-}
-/**
- * ruta:tablas/Prueba.txt
- * fs asi llame la variable
- * lo ultimo de err es callback
- */
-fs.writeFile(`tablas/PruebaBase${base}.txt`, data, (err) => {
-    //si hay un error
-    if (err) throw err;
-    //sino sigue
-    console.log(`El archivo fue creado de la tabla ${base}`);
-  });
+
+crearArchivo(base).then(archivo => {
+    console.log(archivo);
+}).catch(err => console.log(err));
---
+++ Bases/multiplicar/multiplicar.js
@@ -0,0 +1,35 @@
+//require
+const fs = require('fs');
+//datos
+  //Promesa que va a ser llamada en otros archivos
+  let crearArchivo = (base) => {
+      return new Promise((resolve,reject)=>{
+          //Verifica si es un numero
+        if(!Number(base)) {reject(`No es '${base}'un numero `)}
+        else{
+            let data ='';
+            for(let i = 1; i<= 10; i++){
+                //esto guarda las multiplicaciones
+            data += (`${base} * ${i} = ${base*i} \n`);
+            }
+            /**
+             * ruta:tablas/Prueba.txt
+             * fs asi llame la variable
+             * lo ultimo de err es callback
+             */
+            fs.writeFile(`tablas/PruebaBase${base}.txt`, data, (err) => {
+                //si hay un error
+                if (err) reject(err);
+                //sino 
+                else resolve(`PruebaBase${base}.txt`);
+            });
+        }
+        
+      });
+  }
+
+  ///para que puedas exportar la funciones que quieras
+module.exports = {
+    //por ES6 se puede dejar asi pero porque tiene el mismo nombre sino crearArchivo : crearArchivo
+    crearArchivo
+}
