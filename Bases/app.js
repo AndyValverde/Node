c6d1a8c25204a090e05a9b85e361929546606322
AndyValverde <lupy29@hotmail.com>
2021-01-27 21:54:03 -0600

    required paquetes File System

--- Bases/app.js
+++ Bases/app.js
@@ -1,4 +1,20 @@
-let base = 2;
+//require
+const fs = require('fs');
+//datos
+let base = 6;
+let data ='';
 for(let i = 1; i<= 10; i++){
-    console.log(`${base} * ${i} = ${base*i}`);
+    //esto guarda las multiplicaciones
+data += (`${base} * ${i} = ${base*i} \n`);
 }
+/**
+ * ruta:tablas/Prueba.txt
+ * fs asi llame la variable
+ * lo ultimo de err es callback
+ */
+fs.writeFile(`tablas/PruebaBase${base}.txt`, data, (err) => {
+    //si hay un error
+    if (err) throw err;
+    //sino sigue
+    console.log(`El archivo fue creado de la tabla ${base}`);
+  });
