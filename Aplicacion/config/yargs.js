67e12c27514d06046433aa9947eddfcf2dba5d66
AndyValverde <lupy29@hotmail.com>
2021-01-29 17:33:57 -0600

    optimizacion del yargs

--- Aplicacion/config/yargs.js
+++ Aplicacion/config/yargs.js
@@ -1,27 +1,21 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+}
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marcado como completado o pendiente la tarea'
+}
 const argv = require('yargs').
 command('crear','Crear un elemento por hacer',{
-    descripcion : {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea por hacer'
-    }
+    descripcion 
 }).command('actualizar', 'Actualizar el estado completado de una tarea',{
-    descripcion : {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea por hacer'
-    },
-    completado : {
-        alias: 'c',
-        default: true,
-        desc: 'Marcado como completado o pendiente la tarea'
-    }
+    descripcion ,
+    completado 
 }).command('borrar',"Borrar un elemento de la lista",{
-    descripcion : {
-        demand: true,
-        alias: 'd',
-        desc: 'Descripcion de la tarea por hacer'
-    }
+    descripcion 
 }).help().argv;
 
 module.exports = {
